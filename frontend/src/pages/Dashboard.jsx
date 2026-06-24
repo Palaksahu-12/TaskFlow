f74cc943425620633a0ae0eb39baf8b60a21ec1e
Palak Sahu <palaksahu2233@gmail.com>
2026-06-24 21:19:29 +0530

    Updated dashboard, profile, projects and sidebar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,34 +5,68 @@ const Dashboard = () => {
     {
       title: "Total Projects",
       value: 12,
+      icon: "📁",
     },
     {
       title: "Total Tasks",
       value: 48,
+      icon: "📝",
     },
     {
       title:"Comleted Tasks",
       value: 30,
+      icon: "✅",
     },
     {
       title:"Pending Tasks",
       value: 18,
+      icon: "⏳",
+
     },
   ];
 
+  const recentProjects = [
+    {
+      id: 1,
+      name:"TasFlow",
+      status: "In Progress",
+      tasks: 12,
+    },
+    {
+      id:2,
+      name:"Portfolio Website",
+      status:"Completed",
+      tasks:8,
+    },
+    {
+      id:3,
+      name:"E-Commerce App",
+      status: "Planing",
+      tasks:20,
+    }
+  ]
+
   return (
     <Layout>
-      <div>
+      <div className="spacce-y-8">
+        {/* Welcome Section */}
+        <div className="bg-gradient-to-r from-blue-600 to-indigo-600 text-white rounded-2xl p-8 shadow-lg">
         <h1 className="text-3xl font-bold text-slate-800 mb-6">
-          Dashboard Page
+          Welcome Back, John 👋
         </h1>
+        <p className="mt-3 text-lg text-blue-100">
+          Manage your projects and track your progress efficiently.
+        </p>
+        </div>
 
+        {/* Statistics Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {stats.map((stat) =>(
           <div
           key={stat.title}
-          className="bg-white p-6 rounded-lg shadow-md"
+          className="bg-white p-6 rounded-2lg shadow-md hover:shadow-xl transition duration-300"
           >
+            <div className="text-5xl">{stat.icon}</div>
             <h2 className="text-gray-500 text-sm">
               {stat.title}
             </h2>
@@ -43,17 +77,62 @@ const Dashboard = () => {
           ))}
         </div>
 
-        <div className="mt-8 bg-white rounded-xl shadow-md p-6">
-          <h2 className="text-xl font-semibold mb-4">
+        {/* Recent Projects */}
+        <div className="bg-white rounded-2xl shadow-md p-6">
+          <h2 className="text-2xl font-semibold mb-6">
+            Recent Projects
+          </h2>
+
+          <div className-y-4>
+            {recentProjects.map((project) => (
+              <div
+               key={project.id}
+               className="flex justify-between items-center border border-gray-200 rounded-xl p-4 hover:bg-slate-50 transition"
+              >
+                <div>
+                  <h3 className="font-semibold text-lg">
+                    {project.name}
+                  </h3>
+                  <p className="text-gray-500">
+                    {project.tasks} Tasks
+                  </p>
+                </div>
+
+                <span className="bg-blue-100 text-blue-700 px-4 py-1 rounded-full text-sm font-medium">
+                  {project.status}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Recent Activity */}
+        <div className="bg-white rounded-2xl shadow-md p-6">
+          <h2 className="text-2xl font-semibold mb-6">
             Recent Activity
           </h2>
 
-          <ul className="space-y-3">
-            <li>✅ Task "Design Homepage" completed</li>
-            <li>📁 New Project "TaskFlow" created</li>
-            <li>📝 Task "Setup Backend" assigned</li>
-            <li>🚀 Project "Portfolio Website" updated</li>
+          <ul className="space-y-4">
+            <li className="border-1-4 border-green-500 pl-4">
+              ✅ ompleted task "Design Homepage"
+            </li>
+            <li className="border-1-4 border-green-500 pl-4">
+              📁 Created new project "TaskFlow"
+            </li>
+            <li className="border-1-4 border-green-500 pl-4">
+              📝 Added task "Setup Backend API"
+            </li>
+            <li className="border-1-4 border-green-500 pl-4"> 
+              🚀 Updated project "Portfolio Website"
+            </li>
           </ul>
+        </div>
+
+        {/* Quote Section */}
+        <div className="bg-indigo-100 rounded-2xl p-6 shadow-md">
+          <p className="text-xl italic text-indigo-800">
+            "Productivity is never an accident. It is always the result of commitment, intelligent planning, and focused effort."
+          </p>
         </div>
       </div>
     </Layout>
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -8,20 +8,23 @@ const Projects = () => {
     {
       id: 1,
       title: "TaskFlow",
-      description:"Project Management Tool",
+      description:"Productivity is never an accident. It is always the result of commitment, intelligent planning, and focused effort.",
       tasks:12,
+      status: "In Progress",
     },
     {
       id: 2,
       title: "Portfolio Website",
-      description:"Personal portfolio project",
+      description:"Personal portfolio website showcasing projects and skills.",
       tasks: 8,
+      status: "Completed",
     },
     {
       id: 3,
       title:"E-Commerce App",
-      description:"Online shopping platform",
+      description:"Online shopping platform with cart and payment features.",
       tasks: 20,
+      status: "Completed",
     },
   ];
 
@@ -39,9 +42,9 @@ const Projects = () => {
       title: projectName,
       description: projectDescription,
       tasks: 0,
+      status: "Planning",
     }
     setProjectDescription([...projects, newProject]);
-
     setProjectName("");
     setProjectDescription("");
     setShowModal(false);
@@ -49,44 +52,69 @@ const Projects = () => {
 
   return (
     <Layout>
-      <div>
-        <div className="flex justify-between items-center mb-6">
+      <div className="space-y-8">
+        {/* Header */}
+        <div className="flex justify-between items-center">
+          <div>
           <h1 className="text-3xl font-bold text-slate-800">
             Projects 
           </h1>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+          <p className="text-gray-500 mt-2">
+            Manage and track all your projects.
+          </p>
+          </div>
+
+          <button 
+          onClick={() => setShowModal(true)}
+          className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
             + New Project
           </button>
         </div>
 
+        {/* Prroject Cards */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project) => (
             <div
             key={project.id}
-            className="bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition"
+            className="bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition duration-300"
             >
-              <h2 className="text-xl font-semibold mb-2">
+              <div className="flex justify-between items-start">
+                <h2 className="text-2xl font-semibold text-slate-800">
                 {project.title}
-              </h2>
+                </h2>
+
+                <span className="bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-sm">
+                {project.status}
+                </span>
+              </div>
+
               <p className="text-gray-600 mb-4">
                 {project.description}
               </p>
-              <p className="text-sm text-gray-500 mb-4">
-                Tasks: {project.tasks}
-              </p>
-              <button onClick={() => navigate(`/projects/${project.id}`)}
-               className="w-full bg-slate-900 text-white py-2 rounded-lg hover:bg-slate-800"  
-              >
-                View Project
-              </button>
+
+              <div className="mt-6 flex justify-between items-center">
+                <div>
+                  <p className="text-sm text-gray-500">Tasks</p>
+                  <p className="text-xl font-bold">
+                    Tasks: {project.tasks}
+                  </p>
+                </div>
+
+                <button onClick={() => navigate(`/projects/${project.id}`)}
+                className="w-full bg-slate-900 text-white py-2 rounded-lg hover:bg-slate-800"  
+                >
+                 View 
+                </button>
+              </div>
             </div>
           ))}
         </div>
         
+        {/* Modal */}
         {showModal && (
           <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
-            <div className="bg-white p-6 rounded-xl w-full max-w-md">
-              <h2 className="text-2xl font-bold mb-4">
+            <div className="bg-white p-6 rounded-2xl w-full max-w-md">
+              <h2 className="text-2xl font-bold mb-6">
                 Create New Project
               </h2>
 
@@ -95,7 +123,8 @@ const Projects = () => {
               placeholder="Project Name"
               value={projectName}
               onChange={(e) => setProjectName(e.target.value)}
-              className="w-full border p-3 rounded-lg mb-4"/>
+              className="w-full border border-gray-300 p-3 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+
               <textarea
               placeholder="Project Description"
               rows="4"
@@ -103,7 +132,7 @@ const Projects = () => {
               onChange={(e) =>
                 setProjectDescription(e.target.value)
               }
-              className="w-full border p-3 rounded-lg mb-4"
+              className="w-full border border-gray-300 p-3 rounded-lg mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
 
               <div className="flex justify-end gap-3">
